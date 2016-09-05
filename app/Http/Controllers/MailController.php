3f4d93f587b97a850f199cbbfe825cf46b80f754
Elandlord <e.landheer@mentechmedia.nl>
2016-09-05 16:40:06 +0200

    Contactformuliertje
    DSSDFSDF

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -21,4 +21,25 @@ class MailController extends Controller
                 ->subject('Bedankt voor je inschrijving op HMV Actis!');
         });
    }
+
+   public function contactMail(Request $request)
+   {
+
+    $name = $request->input('voornaam') . " " . $request->input('achternaam');
+    $email = $request->input('emailadres');
+
+    $telefoonnummer = $request->input('telefoonnummer');
+    $bericht = $request->input('bericht');
+
+    $beautymail = app()->make(\Snowfire\Beautymail\Beautymail::class);
+        $beautymail->send('emails.contact', ['telefoonnummer' => $telefoonnummer, 'bericht' => $bericht], function($message) use ($name, $email)
+        {
+            $message
+                ->from($email, $name)
+                ->to('bestuur@hmvactis.nl', 'HMV Actis')
+                ->subject('Contactbericht van ' . $name);
+        });
+
+        return redirect('/');
+   }
 }
