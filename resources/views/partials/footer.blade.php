e586770bb9ece721ce8fb4bfbb2e8a2c8ce81207
Melvin Tehubijuluw <melvin.tehubijuluw@beeproger.com>
2016-12-28 17:01:23 +0100

    Beleidsplan

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -43,9 +43,9 @@
 			</div>
 
 			<div class="col-lg-6">
-				<a class="text-color-light block space-outside-xs" href="Algemene voorwaarden website.pdf">  Algemene voorwaarden </a>
+				<a class="text-color-light block space-outside-xs" href="Algemene voorwaarden lidmaatschap HMV Actis.pdf">  Algemene voorwaarden </a>
 				<a class="text-color-light block space-outside-xs" href="#"> Sitemap </a>
-				<a class="text-color-light block space-outside-xs" href="../../beleidsplan_definitief_15-16.docx"> Beleidsplan </a>
+				<a class="text-color-light block space-outside-xs" href="../../Beleidsplanversiedefinitief.pdf"> Beleidsplan </a>
 				<a class="text-color-light block space-outside-xs" href="../../huishoudelijkreglement-2012.pdf"> Huishoudelijk Regelement</a>
 			</div>
 
